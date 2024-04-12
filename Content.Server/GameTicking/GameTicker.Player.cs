621d12522b002c44b6448b91e35ed33d87978f74
Jerry <48524572+JerryImMouse@users.noreply.github.com>
2024-04-12 20:28:04 +0500

    restore player joining

--- Content.Server/GameTicking/GameTicker.Player.cs
+++ Content.Server/GameTicking/GameTicker.Player.cs
@@ -58,7 +58,8 @@ namespace Content.Server.GameTicking
                     }
 
                     // Make the player actually join the game.
-                    // timer time must be > tick length
+                    // timer time must be > tick length 
+                    Timer.Spawn(0, () => _playerManager.JoinGame(args.Session));
 
                     var record = await _dbManager.GetPlayerRecordByUserId(args.Session.UserId);
                     var firstConnection = record != null &&
